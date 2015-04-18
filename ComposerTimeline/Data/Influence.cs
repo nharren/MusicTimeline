8b68247749d3e689a74ffd8142faedcd3c4851d2
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2015-04-17 21:01:42 -0700

    Addition to last commit.

---
+++ ComposerTimeline/Data/Influence.cs
@@ -0,0 +1,28 @@
+﻿using Database;
+using System.Windows.Input;
+
+namespace NathanHarrenstein.ComposerTimeline
+{
+    public class Influence
+    {
+        public ICommand Click
+        {
+            get; set;
+        }
+
+        public Composer Composer
+        {
+            get; set;
+        }
+
+        public bool IsEnabled
+        {
+            get; set;
+        }
+
+        public string Name
+        {
+            get; set;
+        }
+    }
+}
