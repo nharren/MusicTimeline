57a45ea6a50889f37f8c3dc40d2cbffda016834e
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2015-07-07 19:44:28 -0700

    Changed PanGrid to hover pan only when shift is pressed.

--- Timeline/PanGrid.cs
+++ Timeline/PanGrid.cs
@@ -9,24 +9,20 @@ namespace NathanHarrenstein.Timeline
 {
     public class PanGrid : Grid
     {
-        private enum Speed { Slow, Fast };
-
-        private Point origin;
-        private MouseHook mouseHook;
-        private Timer lTimer;
-        private Timer rTimer;
-        private Timer tTimer;
-        private Timer bTimer;
-        private Timer tlTimer;
-        private Timer blTimer;
-        private Timer trTimer;
-        private Timer brTimer;
-        private Speed currentSpeed;
-        private Speed newSpeed;
-        private Timer currentTimer;
-        private const int SlowPanInterval = 15;
-        private const int SideZoneSize = 75;
         private const int CornerZoneSize = 150;
+        private const int PanInterval = 30;
+        private const int SideZoneSize = 75;
+        private Timer blTimer;
+        private Timer brTimer;
+        private Timer bTimer;
+        private Timer currentTimer;
+        private Timer lTimer;
+        private MouseHook mouseHook;
+        private Point origin;
+        private Timer rTimer;
+        private Timer tlTimer;
+        private Timer trTimer;
+        private Timer tTimer;
 
         public PanGrid()
         {
@@ -78,77 +74,21 @@ namespace NathanHarrenstein.Timeline
             }
         }
 
-        private void HoverPan(ref Timer timer, Point origin, Vector vector)
-        {
-            if (timer == null)
-            {
-                if (currentTimer != null)
-                {
-                    currentSpeed = Speed.Slow;
-                }
-
-                Action<object> action = o =>
-                {
-                    Dispatcher.BeginInvoke(DispatcherPriority.Input, new ThreadStart(() =>
-                    {
-                        Origin = origin;
-
-                        if (newSpeed != currentSpeed)
-                        {
-                            if (newSpeed == Speed.Fast)
-                            {
-                                vector = new Vector(vector.X * 2, vector.Y * 2);
-                                Console.WriteLine(vector.ToString());
-
-                                currentSpeed = Speed.Fast;
-                            }
-                            else
-                            {
-                                vector = new Vector(vector.X * 0.5, vector.Y * 0.5);
-                                Console.WriteLine(vector.ToString());
-
-                                currentSpeed = Speed.Slow;
-                            }
-                        }
-
-                        Pan(vector);
-                    }));
-                };
-
-                timer = new Timer(new TimerCallback(action), null, 0, 25);
-
-                currentTimer = timer;
-            }
-        }
-
-        protected override void OnMouseWheel(MouseWheelEventArgs e)
-        {
-            Origin = e.GetPosition(this);
-
-            Pan(new Vector(0, -e.Delta));
-
-            base.OnMouseWheel(e);
-        }
-
-        private void mouseHook_MouseHorizontalWheel(object sender, MouseHorizontalWheelEventArgs e)
-        {
-            Origin = new Point(e.X, e.Y);
-
-            Pan(new Vector(e.Delta, 0));
-        }
-
-        protected override void OnKeyDown(KeyEventArgs e)
-        {
-            newSpeed = (Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift ? Speed.Fast : Speed.Slow;
-
-            base.OnKeyDown(e);
-        }
-
         protected override void OnKeyUp(KeyEventArgs e)
         {
-            newSpeed = (Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift ? Speed.Fast : Speed.Slow;
+            if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
+            {
+                EndTimers();
+            }
 
             base.OnKeyUp(e);
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            EndTimers();
+
+            base.OnMouseLeave(e);
         }
 
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
@@ -177,7 +117,158 @@ namespace NathanHarrenstein.Timeline
             base.OnMouseLeftButtonUp(e);
         }
 
-        protected override void OnMouseLeave(MouseEventArgs e)
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            // Necessary for keyboard events.
+            if (!HasEffectiveKeyboardFocus)
+            {
+                Focus();
+            }
+
+            var position = e.GetPosition(this);
+
+            // Click-And-Drag Pan
+            if (IsMouseCaptured)
+            {
+                var vector = Origin - position;
+
+                Pan(vector);
+
+                origin = position;
+
+                return;
+            }
+
+            if (Keyboard.Modifiers.HasFlag(ModifierKeys.Shift))
+            {
+                // Determine Hover Zone
+                var inTopLeftZone = position.Y > 50 && position.Y <= 50 + CornerZoneSize && position.X <= CornerZoneSize;
+                var inTopRightZone = position.Y > 50 && position.Y <= 50 + CornerZoneSize && position.X >= ActualWidth - CornerZoneSize;
+                var inBottomLeftZone = position.Y >= ActualHeight - CornerZoneSize && position.X <= CornerZoneSize;
+                var inBottomRightZone = position.Y >= ActualHeight - CornerZoneSize && position.X >= ActualWidth - CornerZoneSize;
+                var inLeftZone = position.X <= SideZoneSize && !inTopLeftZone && !inBottomLeftZone;
+                var inRightZone = position.X >= ActualWidth - SideZoneSize && !inTopRightZone && !inBottomRightZone;
+                var inBottomZone = position.Y >= ActualHeight - SideZoneSize && !inBottomRightZone && !inBottomLeftZone;
+                var inTopZone = position.Y > 50 && position.Y <= 50 + SideZoneSize && !inTopLeftZone && !inTopRightZone;
+
+                // Left Hover Pan
+                if (inLeftZone && !inBottomLeftZone && !inTopLeftZone)
+                {
+                    HoverPan(ref lTimer, position, new Vector(-PanInterval, 0));
+                    Cursor = Cursors.ScrollW;
+                }
+                else if (lTimer != null)
+                {
+                    lTimer.Dispose();
+                    lTimer = null;
+                    Cursor = Cursors.Arrow;
+                }
+
+                // Right Hover Pan
+                if (inRightZone && !inBottomRightZone && !inTopRightZone)
+                {
+                    HoverPan(ref rTimer, position, new Vector(PanInterval, 0));
+                    Cursor = Cursors.ScrollE;
+                }
+                else if (rTimer != null)
+                {
+                    rTimer.Dispose();
+                    rTimer = null;
+                    Cursor = Cursors.Arrow;
+                }
+
+                // Bottom Hover Pan
+                if (inBottomZone && !inBottomLeftZone && !inBottomRightZone)
+                {
+                    HoverPan(ref bTimer, position, new Vector(0, PanInterval));
+                    Cursor = Cursors.ScrollS;
+                }
+                else if (bTimer != null)
+                {
+                    bTimer.Dispose();
+                    bTimer = null;
+                    Cursor = Cursors.Arrow;
+                }
+
+                // Top Hover Pan
+                if (inTopZone && !inTopLeftZone && !inTopRightZone)
+                {
+                    HoverPan(ref tTimer, position, new Vector(0, -PanInterval));
+                    Cursor = Cursors.ScrollN;
+                }
+                else if (tTimer != null)
+                {
+                    tTimer.Dispose();
+                    tTimer = null;
+                    Cursor = Cursors.Arrow;
+                }
+
+                // Top-Left Hover Pan
+                if (inTopLeftZone)
+                {
+                    HoverPan(ref tlTimer, position, new Vector(-PanInterval + 5, -PanInterval + 5));
+                    Cursor = Cursors.ScrollNW;
+                }
+                else if (tlTimer != null)
+                {
+                    tlTimer.Dispose();
+                    tlTimer = null;
+                    Cursor = Cursors.Arrow;
+                }
+
+                // Bottom-Left Hover Pan
+                if (inBottomLeftZone)
+                {
+                    HoverPan(ref blTimer, position, new Vector(-PanInterval + 5, PanInterval - 5));
+                    Cursor = Cursors.ScrollSW;
+                }
+                else if (blTimer != null)
+                {
+                    blTimer.Dispose();
+                    blTimer = null;
+                    Cursor = Cursors.Arrow;
+                }
+
+                // Top-Right Hover Pan
+                if (inTopRightZone)
+                {
+                    HoverPan(ref trTimer, position, new Vector(PanInterval - 5, -PanInterval + 5));
+                    Cursor = Cursors.ScrollNE;
+                }
+                else if (trTimer != null)
+                {
+                    trTimer.Dispose();
+                    trTimer = null;
+                    Cursor = Cursors.Arrow;
+                }
+
+                // Bottom-Right Hover Pan
+                if (inBottomRightZone)
+                {
+                    HoverPan(ref brTimer, position, new Vector(PanInterval - 5, PanInterval - 5));
+                    Cursor = Cursors.ScrollSE;
+                }
+                else if (brTimer != null)
+                {
+                    brTimer.Dispose();
+                    brTimer = null;
+                    Cursor = Cursors.Arrow;
+                }
+            }
+
+            base.OnMouseMove(e);
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            Origin = e.GetPosition(this);
+
+            Pan(new Vector(0, -e.Delta));
+
+            base.OnMouseWheel(e);
+        }
+
+        private void EndTimers()
         {
             if (lTimer != null)
             {
@@ -221,147 +312,33 @@ namespace NathanHarrenstein.Timeline
             }
 
             Cursor = Cursors.Arrow;
-
-            base.OnMouseLeave(e);
         }
 
-        protected override void OnMouseMove(MouseEventArgs e)
+        private void HoverPan(ref Timer timer, Point origin, Vector vector)
         {
-            // Necessary for keyboard events.
-            if (!HasEffectiveKeyboardFocus)
+            if (timer == null)
             {
-                Focus();
-            }
+                Action<object> action = o =>
+                {
+                    Dispatcher.BeginInvoke(DispatcherPriority.Input, new ThreadStart(() =>
+                    {
+                        Origin = origin;
 
-            var position = e.GetPosition(this);
+                        Pan(vector);
+                    }));
+                };
 
-            // Click-And-Drag Pan
-            if (IsMouseCaptured)
-            {
-                var vector = Origin - position;
+                timer = new Timer(new TimerCallback(action), null, 0, 25);
 
-                Pan(vector);
+                currentTimer = timer;
+            }
+        }
 
-                origin = position;
+        private void mouseHook_MouseHorizontalWheel(object sender, MouseHorizontalWheelEventArgs e)
+        {
+            Origin = new Point(e.X, e.Y);
 
-                return;
-            }
-
-            // Determine Hover Zone
-            var inTopLeftZone = position.Y > 50 && position.Y <= 50 + CornerZoneSize && position.X <= CornerZoneSize;
-            var inTopRightZone = position.Y > 50 && position.Y <= 50 + CornerZoneSize && position.X >= ActualWidth - CornerZoneSize;
-            var inBottomLeftZone = position.Y >= ActualHeight - CornerZoneSize && position.X <= CornerZoneSize;
-            var inBottomRightZone = position.Y >= ActualHeight - CornerZoneSize && position.X >= ActualWidth - CornerZoneSize;
-            var inLeftZone = position.X <= SideZoneSize && !inTopLeftZone && !inBottomLeftZone;
-            var inRightZone = position.X >= ActualWidth - SideZoneSize && !inTopRightZone && !inBottomRightZone;
-            var inBottomZone = position.Y >= ActualHeight - SideZoneSize && !inBottomRightZone && !inBottomLeftZone;
-            var inTopZone = position.Y > 50 && position.Y <= 50 + SideZoneSize && !inTopLeftZone && !inTopRightZone;
-
-            // Left Hover Pan
-            if (inLeftZone && !inBottomLeftZone && !inTopLeftZone)
-            {
-                HoverPan(ref lTimer, position, new Vector(-SlowPanInterval, 0));
-                Cursor = Cursors.ScrollW;
-            }
-            else if (lTimer != null)
-            {
-                lTimer.Dispose();
-                lTimer = null;
-                Cursor = Cursors.Arrow;
-            }
-
-            // Right Hover Pan
-            if (inRightZone && !inBottomRightZone && !inTopRightZone)
-            {
-                HoverPan(ref rTimer, position, new Vector(SlowPanInterval, 0));
-                Cursor = Cursors.ScrollE;
-            }
-            else if (rTimer != null)
-            {
-                rTimer.Dispose();
-                rTimer = null;
-                Cursor = Cursors.Arrow;
-            }
-
-            // Bottom Hover Pan
-            if (inBottomZone && !inBottomLeftZone && !inBottomRightZone)
-            {
-                HoverPan(ref bTimer, position, new Vector(0, SlowPanInterval));
-                Cursor = Cursors.ScrollS;
-            }
-            else if (bTimer != null)
-            {
-                bTimer.Dispose();
-                bTimer = null;
-                Cursor = Cursors.Arrow;
-            }
-
-            // Top Hover Pan
-            if (inTopZone && !inTopLeftZone && !inTopRightZone)
-            {
-                HoverPan(ref tTimer, position, new Vector(0, -SlowPanInterval));
-                Cursor = Cursors.ScrollN;
-            }
-            else if (tTimer != null)
-            {
-                tTimer.Dispose();
-                tTimer = null;
-                Cursor = Cursors.Arrow;
-            }
-
-            // Top-Left Hover Pan
-            if (inTopLeftZone)
-            {
-                HoverPan(ref tlTimer, position, new Vector(-SlowPanInterval + 5, -SlowPanInterval + 5));
-                Cursor = Cursors.ScrollNW;
-            }
-            else if (tlTimer != null)
-            {
-                tlTimer.Dispose();
-                tlTimer = null;
-                Cursor = Cursors.Arrow;
-            }
-
-            // Bottom-Left Hover Pan
-            if (inBottomLeftZone)
-            {
-                HoverPan(ref blTimer, position, new Vector(-SlowPanInterval + 5, SlowPanInterval - 5));
-                Cursor = Cursors.ScrollSW;
-            }
-            else if (blTimer != null)
-            {
-                blTimer.Dispose();
-                blTimer = null;
-                Cursor = Cursors.Arrow;
-            }
-
-            // Top-Right Hover Pan
-            if (inTopRightZone)
-            {
-                HoverPan(ref trTimer, position, new Vector(SlowPanInterval - 5, -SlowPanInterval + 5));
-                Cursor = Cursors.ScrollNE;
-            }
-            else if (trTimer != null)
-            {
-                trTimer.Dispose();
-                trTimer = null;
-                Cursor = Cursors.Arrow;
-            }
-
-            // Bottom-Right Hover Pan
-            if (inBottomRightZone)
-            {
-                HoverPan(ref brTimer, position, new Vector(SlowPanInterval - 5, SlowPanInterval - 5));
-                Cursor = Cursors.ScrollSE;
-            }
-            else if (brTimer != null)
-            {
-                brTimer.Dispose();
-                brTimer = null;
-                Cursor = Cursors.Arrow;
-            }
-
-            base.OnMouseMove(e);
+            Pan(new Vector(e.Delta, 0));
         }
     }
 }
