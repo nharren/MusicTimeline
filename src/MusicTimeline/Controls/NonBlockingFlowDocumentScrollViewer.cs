a2a386b52de238be59aa72fac8e89b4cd78f6f6c
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2015-07-29 23:16:24 -0700

    Added NonBlockingFlowDocumentScrollViewer to prevent it from handling MouseWheel events when vertical scrolling is disabled.

---
+++ src/MusicTimeline/Controls/NonBlockingFlowDocumentScrollViewer.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace NathanHarrenstein.MusicTimeline.Controls
+{
+    public class NonBlockingFlowDocumentScrollViewer : FlowDocumentScrollViewer
+    {
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            if (VerticalScrollBarVisibility != ScrollBarVisibility.Disabled)
+            {
+                base.OnMouseWheel(e);
+            }
+        }
+    }
+}
