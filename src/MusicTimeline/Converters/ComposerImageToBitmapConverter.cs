ca20e25bb9d671b351669c7ba16cd39aef8186ea
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2015-07-30 20:02:02 -0700

    Performance improvements on ComposerPage.

---
+++ src/MusicTimeline/Converters/ComposerImageToBitmapConverter.cs
@@ -0,0 +1,50 @@
+﻿using NathanHarrenstein.MusicDB;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace NathanHarrenstein.MusicTimeline.Converters
+{
+    internal class ComposerImageToBitmapConverter : IValueConverter
+    {
+        private Dictionary<short, BitmapImage> _bitmapCache = new Dictionary<short, BitmapImage>();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ComposerImageToBitmap((ComposerImage)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private BitmapImage ComposerImageToBitmap(ComposerImage composerImage)
+        {
+            if (composerImage == null)
+            {
+                return null;
+            }
+
+            var bitmap = (BitmapImage)null;
+
+            if (!_bitmapCache.TryGetValue(composerImage.ID, out bitmap))
+            {
+                bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.StreamSource = new MemoryStream(composerImage.Image);
+                bitmap.EndInit();
+                bitmap.Freeze();
+
+                _bitmapCache[composerImage.ID] = bitmap;
+
+                return bitmap;
+            }
+
+            return bitmap;
+        }
+    }
+}
---
+++ src/MusicTimeline/Converters/UrlToFaviconConverter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace NathanHarrenstein.MusicTimeline.Converters
+{
+    public class UrlToFaviconConverter : IValueConverter
+    {
+        private static readonly Dictionary<string, BitmapImage> _faviconCache = new Dictionary<string, BitmapImage>();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return UrlToFavicon((string)value);
+        }
+
+        private BitmapImage UrlToFavicon(string url)
+        {
+            var favicon = (BitmapImage)null;
+            var uri = (Uri)null;
+
+            if (!Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out uri))
+            {
+                throw new ArgumentException($"A uri could not be created from the url: {url}");
+            }
+
+            if (_faviconCache.TryGetValue(uri.Host, out favicon))
+            {
+                return favicon;
+            }
+
+            var faviconPath = $@"{Environment.CurrentDirectory}\Resources\Favicons\{uri.Host}.ico";
+
+            if (File.Exists(faviconPath))
+            {
+                favicon = new BitmapImage(new Uri(faviconPath));
+                _faviconCache[url] = favicon;
+
+                return favicon;
+            }
+            else
+            {
+                var faviconUrl = $"http://{uri.Host}/favicon.ico";
+                var faviconUri = (Uri)null;
+
+                if (Uri.TryCreate(faviconUrl, UriKind.Absolute, out faviconUri))
+                {
+                    try
+                    {
+                        favicon = new BitmapImage(faviconUri);
+                        _faviconCache[url] = favicon;
+
+                        return favicon;
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+
+                if (_faviconCache.TryGetValue("", out favicon))
+                {
+                    return favicon;
+                }
+                else
+                {
+                    faviconUrl = $@"{Environment.CurrentDirectory}\Resources\Favicons\Default.ico";
+                    faviconUri = new Uri(faviconUrl);
+                    favicon = new BitmapImage(faviconUri);
+                    _faviconCache[url] = favicon;
+
+                    return favicon;
+                }
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
